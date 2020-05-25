67191beefca4aef27e168050084a9b347a1acdf1
Slava Beloglazov <v.beloglazov@xsolla.com>
2020-05-25 10:08:29 +0500

    add more images

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,16 @@
   import { onDestroy } from 'svelte';
   import praiseImg1 from './images/praise/1.png';
   import praiseImg2 from './images/praise/2.png';
+  import praiseImg3 from './images/praise/3.png';
+  import praiseImg4 from './images/praise/4.png';
+  import praiseImg5 from './images/praise/5.png';
+  import praiseImg6 from './images/praise/6.png';
+  import praiseImg7 from './images/praise/7.png';
+  import praiseImg8 from './images/praise/8.png';
+  import praiseImg9 from './images/praise/9.png';
+  import praiseImg10 from './images/praise/10.png';
+  import praiseImg11 from './images/praise/11.png';
+  import praiseImg12 from './images/praise/12.png';
 
   const HARE = 'Харе';
   const KRISHNA = 'Кришна';
@@ -130,10 +140,23 @@
     const flooredMax = Math.floor(max);
 
     // The maximum is exclusive and the minimum is inclusive
-    return Math.floor(Math.random() * (flooredMax - ceiledMin)) + ceiledMin; 
+    return Math.floor(Math.random() * (flooredMax - ceiledMin)) + ceiledMin;
   }
 
-  const praiseImageList = [praiseImg1, praiseImg2];
+  const praiseImageList = [
+    praiseImg1,
+    praiseImg2,
+    praiseImg3,
+    praiseImg4,
+    praiseImg5,
+    praiseImg6,
+    praiseImg7,
+    praiseImg8,
+    praiseImg9,
+    praiseImg10,
+    praiseImg11,
+    praiseImg12,
+  ];
   const praiseImg = praiseImageList[getRandomInt(0, praiseImageList.length)];
 
   const startButtonName = 'Старт';
@@ -163,7 +186,7 @@
   }
 
   .counter {
-    font-family: "Courier New", Courier, monospace;
+    font-family: 'Courier New', Courier, monospace;
     font-size: 14px;
     margin-right: 10px;
   }
@@ -264,8 +287,13 @@
   <section class="mantra-box">
     {#if finished}
       <div class="praise-box">
-        <span class="praise-text">Ура! Все круги прочитаны! Молодец!</span>
-        <img class="praise-image" src={praiseImg || ''} alt="Вы молодец!" width="300" height="300" />
+        <span class="praise-text">Ура! Все круги прочитаны!</span>
+        <img
+          class="praise-image"
+          src={praiseImg}
+          alt="Вы молодец!"
+          width="300"
+          height="300" />
       </div>
     {:else}
       <div class="mantra-card">
