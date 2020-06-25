b0dd866f673df7975bc7a05b04202f45ac268b44
Slava Beloglazov <v.beloglazov@xsolla.com>
2020-06-25 14:08:34 +0500

    settings component

--- src/App.svelte
+++ src/App.svelte
@@ -4,10 +4,10 @@
   import Play from './icons/Play.svelte';
   import Pause from './icons/Pause.svelte';
   import Reset from './icons/Reset.svelte';
-  import Timer from './icons/Timer.svelte';
 
   import { tratakaState } from './stores';
-import Praise from './Praise.svelte';
+  import Praise from './Praise.svelte';
+  import Settings from './Settings.svelte';
 
   const HARE = 'Харе';
   const KRISHNA = 'Кришна';
@@ -191,9 +191,7 @@ import Praise from './Praise.svelte';
   const startButtonName = 'Старт';
   const resetButtonName = 'Сброс';
   const pauseButtonName = 'Пауза';
-  const roundsInputLabel = 'Кругов';
-  const timeForRoundInputLabel = 'Время круга';
-  const minutes = 'минут';
+
   const roundsLabel = 'Круги';
   const mantrasLabel = 'Мантры';
   // let wordsLabel = "Слова";
@@ -262,40 +260,6 @@ import Praise from './Praise.svelte';
     opacity: 0;
   }
 
-  .settings {
-    margin-bottom: 24px;
-  }
-
-  .settings-form {
-    width: 100%;
-    display: flex;
-    justify-content: space-evenly;
-    align-items: center;
-  }
-
-  .setting-field {
-    display: flex;
-    align-items: center;
-    margin-bottom: 8px;
-  }
-
-  .setting-field:last-child {
-    margin-bottom: 0;
-  }
-
-  .numeric-input {
-    margin: 0 6px;
-    min-width: 40px;
-    max-width: 50px;
-    border: none;
-    border-radius: 0;
-    border-bottom: 1px solid #000000;
-    text-align: center;
-    background-color: inherit;
-    font-weight: 500;
-    font-size: 1.1em;
-  }
-
   .actions {
     width: 100%;
     display: flex;
@@ -358,36 +322,7 @@ import Praise from './Praise.svelte';
   </section>
 
   {#if paused || inactive}
-    <div class="settings">
-      <form class="settings-form">
-        <label class="setting-field" for="rounds">
-          {roundsInputLabel}:
-          <input
-            class="numeric-input"
-            id="rounds"
-            type="number"
-            inputmode="numeric"
-            pattern="[0-9]*"
-            bind:value={rounds}
-            min="1" />
-        </label>
-        <label class="setting-field" for="timeForRound">
-          <span title={timeForRoundInputLabel}>
-            <Timer width="30px" height="30px" />
-          </span>
-          :
-          <input
-            class="numeric-input"
-            id="timeForRound"
-            type="number"
-            inputmode="numeric"
-            pattern="[0-9]*"
-            bind:value={minutesForRound}
-            min="1" />
-          {minutes}
-        </label>
-      </form>
-    </div>
+    <Settings bind:rounds bind:minutesForRound />
   {/if}
 
   <div class="actions">
---
+++ src/Settings.svelte
@@ -0,0 +1,77 @@
+<script>
+  import Timer from './icons/Timer.svelte';
+
+  export let rounds;
+  export let minutesForRound;
+
+  const roundsInputLabel = 'Кругов';
+  const timeForRoundInputLabel = 'Время круга';
+  const minutes = 'минут';
+</script>
+
+<style>
+  .settings {
+    margin-bottom: 24px;
+  }
+
+  .settings-form {
+    width: 100%;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+  }
+
+  .setting-field {
+    display: flex;
+    align-items: center;
+    margin-bottom: 8px;
+  }
+
+  .setting-field:last-child {
+    margin-bottom: 0;
+  }
+
+  .numeric-input {
+    margin: 0 6px;
+    min-width: 40px;
+    max-width: 50px;
+    border: none;
+    border-radius: 0;
+    border-bottom: 1px solid #000000;
+    text-align: center;
+    background-color: inherit;
+    font-weight: 500;
+    font-size: 1.1em;
+  }
+</style>
+
+<div class="settings">
+  <form class="settings-form">
+    <label class="setting-field" for="rounds">
+      {roundsInputLabel}:
+      <input
+        class="numeric-input"
+        id="rounds"
+        type="number"
+        inputmode="numeric"
+        pattern="[0-9]*"
+        bind:value={rounds}
+        min="1" />
+    </label>
+    <label class="setting-field" for="timeForRound">
+      <span title={timeForRoundInputLabel}>
+        <Timer width="30px" height="30px" />
+      </span>
+      :
+      <input
+        class="numeric-input"
+        id="timeForRound"
+        type="number"
+        inputmode="numeric"
+        pattern="[0-9]*"
+        bind:value={minutesForRound}
+        min="1" />
+      {minutes}
+    </label>
+  </form>
+</div>
